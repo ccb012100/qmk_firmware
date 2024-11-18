14da5cd89ddc9089e64c611aeaf5b00eac99e245
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2024-11-18 15:58:47 -0600

    make f17 & f18 easier to reach

--- keyboards/ergodox_ez/keymaps/ccb012100/keymap.c
+++ keyboards/ergodox_ez/keymaps/ccb012100/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_VOLU,    /* split */     QK_AREP,    KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_EQUAL,
         MEH_T(KC_ESC),  KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                   /* split */                 KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        MEH_T(KC_QUOTE),
         KC_LEFT_SHIFT,  KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_VOLD,    /* split */     QK_REP,     KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RIGHT_SHIFT,
-        KC_F17,         KC_MEH,     RGB_TOG,    KC_LGUI,    KC_LALT,                            /* split */                                 KC_RALT,        KC_RGUI,        KC_APP,         KC_MEH,         KC_F18,
+        KC_LEFT_CTRL,   KC_F17,     RGB_TOG,    KC_LGUI,    KC_LALT,                            /* split */                                 KC_RALT,        KC_RGUI,        KC_APP,         KC_F18,         KC_RIGHT_CTRL,
     //             ⭦ left hand ⭧              ⭩ left thumb cluster ⭨                            /* split */                                             ⭩ right thumb cluster ⭨            ⭦ right hand ⭧
                                             KC_LEFT,         KC_DOWN,                           /* split */                                     KC_UP,              KC_RIGHT,
                                                              KC_HOME,                           /* split */                                     KC_PAGE_UP,
