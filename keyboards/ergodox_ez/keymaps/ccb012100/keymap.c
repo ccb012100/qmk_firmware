352b3740ca5e005bdf2ad915ed1488b7bb3d5199
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2025-03-06 09:33:41 -0600

    add MEH momentary on upper corner keys

--- keyboards/ergodox_ez/keymaps/ccb012100/keymap.c
+++ keyboards/ergodox_ez/keymaps/ccb012100/keymap.c
@@ -18,15 +18,15 @@ enum custom_keycodes {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_ergodox_pretty(
-        KC_GRAVE,       KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_MPLY,    /* split */     KC_PSCR,    KC_6,           KC_7,           KC_8,           KC_9,           KC_0,       KC_BSPC,
-        KC_TAB,         KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_VOLU,    /* split */     QK_AREP,    KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,       KC_EQUAL,
-        KC_ESC,         KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                   /* split */                 KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,    KC_QUOTE,
-        KC_LEFT_SHIFT,  KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_VOLD,    /* split */     QK_REP,     KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,   KC_RIGHT_SHIFT,
-        RGB_TOG,        KC_F17,     KC_MEH,     KC_LGUI,    KC_LALT,                            /* split */                                 KC_RALT,        KC_RGUI,        KC_MEH,         KC_F18,     KC_APP,
-    //             ⭦ left hand ⭧              ⭩ left thumb cluster ⭨                            /* split */                                             ⭩ right thumb cluster ⭨            ⭦ right hand ⭧
-                                            KC_LEFT,         KC_DOWN,                           /* split */                                     KC_UP,              KC_RIGHT,
-                                                             KC_HOME,                           /* split */                                     KC_PAGE_UP,
-                      MT(MOD_LCTL,KC_BSPC), LT(RIGHT,KC_TAB),KC_END,                            /* split */                                     KC_PGDN,            LT(LEFT,KC_ENTER), MT(MOD_RCTL, KC_SPACE)
+        MT(MOD_MEH,KC_GRAVE),   KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_MPLY,    /* split */     KC_PSCR,    KC_6,           KC_7,           KC_8,           KC_9,           KC_0,       MT(MOD_MEH,KC_BSPC),
+        KC_TAB,                 KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_VOLU,    /* split */     QK_AREP,    KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,       KC_EQUAL,
+        KC_ESC,                 KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                   /* split */                 KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,    KC_QUOTE,
+        KC_LEFT_SHIFT,          KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_VOLD,    /* split */     QK_REP,     KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,   KC_RIGHT_SHIFT,
+        KC_F17,                 KC_MEH,     RGB_TOG,    KC_LGUI,    KC_LALT,                            /* split */                                 KC_RALT,        KC_RGUI,        KC_APP,         KC_MEH,     KC_F18,
+    //             ⭦ left hand ⭧              ⭩ left thumb cluster ⭨                                    /* split */                                             ⭩ right thumb cluster ⭨            ⭦ right hand ⭧
+                                            KC_LEFT,         KC_DOWN,                                   /* split */                                     KC_UP,              KC_RIGHT,
+                                                             KC_HOME,                                   /* split */                                     KC_PAGE_UP,
+                      MT(MOD_LCTL,KC_BSPC), LT(RIGHT,KC_TAB),KC_END,                                    /* split */                                     KC_PGDN,            LT(LEFT,KC_ENTER), MT(MOD_RCTL, KC_SPACE)
     ),
     // Layer 1
     [LEFT] = LAYOUT_ergodox_pretty(
